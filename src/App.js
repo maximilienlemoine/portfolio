36893b0a33a53668e46419ceaea4e0a39ec3def2
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-03-20 16:20:41 +0100

    wip : navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavComponent from "./Component/NavComponent";
 
 function App() {
   return (
     <div className="App">
-
+        <NavComponent />
     </div>
   );
 }
