b80c54b9250ca43bd013e296c0c0f0d638404486
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-03-23 14:05:23 +0100

    fixs : meta description & text-center

--- src/Component/SkillsComponent.js
+++ src/Component/SkillsComponent.js
@@ -20,7 +20,7 @@ function SkillsComponent() {
                 <div className={'flex flex-col items-center gap-3'}>
                     <h2 className={'text-3xl md:text-4xl bg-background p-3 text-primary text-center pb-0'}>Compétences</h2>
                     <hr className={'border-primary w-3/12 sm:w-2/12 lg:w-1/12 my-0'}/>
-                    <p>Je n'arrête jamais d'apprendre et de m'améliorer.</p>
+                    <p className={'text-center'}>Je n'arrête jamais d'apprendre et de m'améliorer.</p>
                 </div>
                 <div className={'flex flex-col md:flex-row justify-center flex-wrap items-center gap-8 mt-10'}>
                     <CardSkillComponent skillTitle={'Front-end'} skillDescription={'JavaScript (React, Vue.js), Tailwind'} skillImage={FrontEndIcon}/>
