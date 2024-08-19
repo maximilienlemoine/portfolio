815ada6a8e5a5abc24489b82f4ec57eb4bff0e7b
Maximilien <maximilien.lemoine.pro@gmail.com>
2024-08-19 13:21:42 +0200

    fix : stack cards

--- src/Component/CardStackComponent.js
+++ src/Component/CardStackComponent.js
@@ -4,7 +4,7 @@ function CardStackComponent(props) {
     return (
         <div className={'app-stack-icon-size flex items-center flex-col'}>
             <div className="p-4 rounded-full" style={{backgroundColor: props.color}}>
-                <img src={props.icon} alt={'logo de ' + props.title} className={'w-24 h-24 m-2'}/>
+                <img src={props.icon} alt={'logo de ' + props.title} className={'w-12 h-12 m-2'}/>
             </div>
             <p className={'text-center text-xl ubuntu mt-3'}>{props.title}</p>
         </div>
