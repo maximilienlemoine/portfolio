651f05222a1f0d9f40acdaf24d7a21a7ab576043
Maximilien LEMOINE <maximilien.lemoine.pro@gmail.com>
2025-04-20 11:20:06 +0200

    fix : prettier

--- src/Component/CardMeComponent.js
+++ src/Component/CardMeComponent.js
@@ -7,39 +7,50 @@ import React from "react";
 
 function CardMeComponent() {
     return (
-    <div className={'border-white border card-cv mt-10 md:mt-0'}>
-        <img src={MaximilienImage} alt={'Photographie de Maximilien'} height={200} width={200}
-             className={'rounded-full w-3/5 mt-10 mx-auto border-2 border-primary'}/>
-        <p className={'text-center text-4xl '}>Maximilien</p>
-        <p className={'text-center font-light'}>Développeur Full-Stack</p>
-        <ul className={'p-6'}>
-            <li className={'flex gap-2'}>
-                <img src={LetterIcon} alt={'Icone de lettre'}/>
-                <a href={'mailto:maximilien.lemoine.pro@gmail.com'}>maximilien.lemoine.pro@gmail.com</a>
-            </li>
-            <li className={'flex gap-2'}><img src={LocationIcon} alt={'Icone de localisation'}/> France</li>
-            <li className={'flex gap-2'}>
-                <img src={LinkIcon} alt={'Icone de lien'}/>
-                <a href={'https://www.maximilien-lemoine.fr'} target={'_blank'} rel="noreferrer">www.maximilien-lemoine.fr</a>
-            </li>
-            <li>
-                <ul className={'flex mt-2 justify-evenly'}>
-                    <li className={'bg-primary px-2 rounded-full font-bold text-background'}>PHP</li>
-                    <li className={'bg-primary px-2 rounded-full font-bold text-background'}>JS</li>
-                    <li className={'bg-primary px-2 rounded-full font-bold text-background'}>C#</li>
-                    <li className={'bg-primary px-2 rounded-full font-bold text-background'}>React Native</li>
-                </ul>
-            </li>
-            <li>
-                <button
-                    className={'bg-white text-background ubuntu rounded-full p-2 mt-4 mx-auto flex gap-2 font-bold'}
-                    onClick={() => window.open('https://www.maximilien-lemoine.fr/CV_Maximilien_LEMOINE.pdf', '_blank')}
-                >
-                    Curriculum Vitae <img src={DownloadIcon} alt={'Icone de téléchargement'}/>
-                </button>
-            </li>
-        </ul>
-    </div>
+        <div className={"border-white border card-cv mt-10 md:mt-0"}>
+            <img
+                src={MaximilienImage}
+                alt={"Photographie de Maximilien"}
+                height={200}
+                width={200}
+                className={"rounded-full w-3/5 mt-10 mx-auto border-2 border-primary"}
+            />
+            <p className={"text-center text-4xl "}>Maximilien</p>
+            <p className={"text-center font-light"}>Développeur Full-Stack</p>
+            <ul className={"p-6"}>
+                <li className={"flex gap-2"}>
+                    <img src={LetterIcon} alt={"Icone de lettre"} />
+                    <a href={"mailto:maximilien.lemoine.pro@gmail.com"}>maximilien.lemoine.pro@gmail.com</a>
+                </li>
+                <li className={"flex gap-2"}>
+                    <img src={LocationIcon} alt={"Icone de localisation"} /> France
+                </li>
+                <li className={"flex gap-2"}>
+                    <img src={LinkIcon} alt={"Icone de lien"} />
+                    <a href={"https://www.maximilien-lemoine.fr"} target={"_blank"} rel="noreferrer">
+                        www.maximilien-lemoine.fr
+                    </a>
+                </li>
+                <li>
+                    <ul className={"flex mt-2 justify-evenly"}>
+                        <li className={"bg-primary px-2 rounded-full font-bold text-background"}>PHP</li>
+                        <li className={"bg-primary px-2 rounded-full font-bold text-background"}>JS</li>
+                        <li className={"bg-primary px-2 rounded-full font-bold text-background"}>C#</li>
+                        <li className={"bg-primary px-2 rounded-full font-bold text-background"}>React Native</li>
+                    </ul>
+                </li>
+                <li>
+                    <button
+                        className={"bg-white text-background ubuntu rounded-full p-2 mt-4 mx-auto flex gap-2 font-bold"}
+                        onClick={() =>
+                            window.open("https://www.maximilien-lemoine.fr/CV_Maximilien_LEMOINE.pdf", "_blank")
+                        }
+                    >
+                        Curriculum Vitae <img src={DownloadIcon} alt={"Icone de téléchargement"} />
+                    </button>
+                </li>
+            </ul>
+        </div>
     );
 }
 
--- src/Component/WorksComponent.js
+++ src/Component/WorksComponent.js
@@ -1,9 +1,9 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import CardWorkComponent from "./CardWorkComponent";
 import CircleLeftIcon from "../icons/circle-left.svg";
 import CircleRightIcon from "../icons/circle-right.svg";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import {Carousel} from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
 function WorksComponent() {
     const [works, setWorks] = React.useState([]);
@@ -11,53 +11,53 @@ function WorksComponent() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(process.env.REACT_APP_API_URL + 'private-api/project/get', {
-                    method: 'POST',
+                const response = await fetch(process.env.REACT_APP_API_URL + "private-api/project/get", {
+                    method: "POST",
                     headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': 'Bearer ' + process.env.REACT_APP_API_SECRET
+                        "Content-Type": "application/json",
+                        Authorization: "Bearer " + process.env.REACT_APP_API_SECRET,
                     },
                 });
                 const data = await response.json();
                 setWorks(data);
             } catch (error) {
-                console.error('Error:', error);
+                console.error("Error:", error);
             }
         };
 
         fetchData();
     }, []);
 
-    const renderCenterRightControls = ({nextDisabled, nextSlide}) => (
+    const renderCenterRightControls = ({ nextDisabled, nextSlide }) => (
         <button
-            className={'bg-background p-2 rounded-full mr-3'}
+            className={"bg-background p-2 rounded-full mr-3"}
             disabled={nextDisabled}
             onClick={nextSlide}
             aria-label="Go to next slide"
         >
-            <img src={CircleRightIcon} alt={'Icone de flèche droite'} className={'w-5 md:w-7'}/>
+            <img src={CircleRightIcon} alt={"Icone de flèche droite"} className={"w-5 md:w-7"} />
         </button>
     );
 
-    const renderCenterLeftControls = ({previousDisabled, previousSlide}) => (
+    const renderCenterLeftControls = ({ previousDisabled, previousSlide }) => (
         <button
-            className={'bg-background p-2 rounded-full ml-3'}
+            className={"bg-background p-2 rounded-full ml-3"}
             disabled={previousDisabled}
             onClick={previousSlide}
             aria-label="Go to previous slide"
         >
-            <img src={CircleLeftIcon} alt={'Icone de flèche gauche'} className={'w-5 md:w-7'}/>
+            <img src={CircleLeftIcon} alt={"Icone de flèche gauche"} className={"w-5 md:w-7"} />
         </button>
     );
 
     return (
-        <section className={'bg-backgroundSecondary section py-14'} id='works'>
-            <div className={'flex flex-col items-center gap-5'}>
-                <p className={'text-3xl md:text-4xl p-3 text-primary text-center pb-0'}>Réalisations</p>
-                <hr className={'border-primary w-3/12 sm:w-2/12 lg:w-1/12 my-0'}/>
-                <p className={'text-center'}>Sélection de réalisation disponible.</p>
+        <section className={"bg-backgroundSecondary section py-14"} id="works">
+            <div className={"flex flex-col items-center gap-5"}>
+                <p className={"text-3xl md:text-4xl p-3 text-primary text-center pb-0"}>Réalisations</p>
+                <hr className={"border-primary w-3/12 sm:w-2/12 lg:w-1/12 my-0"} />
+                <p className={"text-center"}>Sélection de réalisation disponible.</p>
             </div>
-            <div className={'flex justify-center '}>
+            <div className={"flex justify-center "}>
                 <Carousel
                     showArrows={true}
                     showStatus={false}
@@ -70,27 +70,27 @@ function WorksComponent() {
                     centerSlidePercentage={100}
                     renderCenterLeftControls={renderCenterLeftControls}
                     renderCenterRightControls={renderCenterRightControls}
-                    className={'w-11/12 md:w-10/12 my-10 md:my-20'}
+                    className={"w-11/12 md:w-10/12 my-10 md:my-20"}
                 >
                     {works.map((work, index) => {
-                        return <CardWorkComponent
-                            key={index}
-                            workTitle={work.title}
-                            workGoal={
-                                work.goals.map((goal, index) => {
-                                    return [goal.title]
+                        return (
+                            <CardWorkComponent
+                                key={index}
+                                workTitle={work.title}
+                                workGoal={work.goals.map((goal, index) => {
+                                    return [goal.title];
                                 })}
-                            workDescription={work.description}
-                            workWebLink={work.projectLink}
-                            workCodeLink={work.sourceCodeLink}
-                            workStack={work.stacks.map((stack, index) => {
-                                return [stack.title, stack.icon]
-                            })}
-                        />
+                                workDescription={work.description}
+                                workWebLink={work.projectLink}
+                                workCodeLink={work.sourceCodeLink}
+                                workStack={work.stacks.map((stack, index) => {
+                                    return [stack.title, stack.icon];
+                                })}
+                            />
+                        );
                     })}
                 </Carousel>
             </div>
-
         </section>
     );
 }
