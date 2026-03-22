a8d7592e8d9f0e75d4b873c4ad3edf2a574ca61b
Maximilien <maximilien.lemoine.pro@gmail.com>
2026-03-22 12:19:32 +0100

    fix: image, about, download

--- src/components/DownloadCV.tsx
+++ src/components/DownloadCV.tsx
@@ -4,10 +4,22 @@ import React from "react";
 import Image from "next/image";
 
 export default function DownloadCV({ cvLink }: { cvLink: string }) {
+    const handleDownload = async () => {
+        const response = await fetch(cvLink);
+        const blob = await response.blob();
+
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "CV_Maximilien_LEMOINE.pdf";
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+
     return (
         <button
+            onClick={handleDownload}
             className={"bg-white text-background ubuntu rounded-full p-2 mt-4 mx-auto flex gap-2 font-bold"}
-            onClick={() => window.open(cvLink, "_blank")}
         >
             Curriculum Vitae
             <Image src={"/icons/download.svg"} alt={"Icone de téléchargement"} width="20" height="20" />
